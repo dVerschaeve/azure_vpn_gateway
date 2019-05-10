9a110f46b21242afd99b823d7641955d91447a86
dVerschaeve <dries.verschaeve@vddesign.be>
2019-05-10 14:37:59 +0200

    Update build.sh
    Bash does not stop for input, removed all automated steps. These need to be executed manually for now

--- build.sh
+++ build.sh
@@ -20,19 +20,8 @@ apt-get -y install docker-ce
 
 ### Deploy the different containers
 echo "Deploying Portainer"
-docker run -d --Name='Portainer' -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v /docker/portainer:/data portainer/portainer --restart always
+docker run -d --name='Portainer' -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v /docker/portainer:/data portainer/portainer --restart always
 echo "Deploying TinyProxy"
-docker run -d --name='Tinyproxy' -v /docker/tinyproxy/tinyproxy.conf:/etc/tinyproxy/tinyproxy.conf -p 443:8888 --restart always dannydirect/tinyproxy:latest ANY
-
-echo "Deploying OpenVPN"
-read -p "OpenVPN URL: "  OpenVPNURL
-echo "OpenVPN server URL: $OpenVPNURL"
-docker run -v /docker/openvpn:/etc/openvpn --log-driver=none --rm kylemanna/openvpn ovpn_genconfig -N -b -d -D -u tcp://$OpenVPNURL -p "route 10.0.0.0 255.255.255.0"
-docker run -v /docker/openvpn:/etc/openvpn --log-driver=none --rm -it kylemanna/openvpn ovpn_initpki
-docker run -v --name='OpenVPN' /docker/openvpn:/etc/openvpn -d -p 1194:1194/tcp --restart always --cap-add=NET_ADMIN kylemanna/openvpn
-
-read -p "OpenVPN Profile Name: " OpenVPNProfile
-docker run -v /docker/openvpn:/etc/openvpn --log-driver=none --rm -it kylemanna/openvpn easyrsa build-client-full $OpenVPNProfile nopass
-docker run -v /docker/openvpn:/etc/openvpn --log-driver=none --rm kylemanna/openvpn ovpn_getclient $OpenVPNProfile > /docker/OpenVPNProfiles/$OpenVPNProfile.ovpn
+docker run -d --name='Tinyproxy' -v /docker/TinyProxy/tinyproxy.conf:/etc/tinyproxy/tinyproxy.conf -p 443:8888 --restart always dannydirect/tinyproxy:latest ANY
 
 echo "All done, enjoy!"
